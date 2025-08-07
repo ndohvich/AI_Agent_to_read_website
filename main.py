0ced2f4c8c32166c9fe072d03de649e11a1c7139
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2025-08-07 18:18:55 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import asyncio
+from pydantic_ai import Agent
+from pydantic_ai.mcp import MCPServerStdio
+
+#define the MCP server
+mcp_fetch_server = MCPServerStdio(
+    command="python",
+    args = ["-m", "mcp_server_fetch"]
+)
+
+#configure groq api key
+os.environ["GROQ_API_KEY"] = "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"
+
+#create the AI Agent
+agent = Agent(
+    model = "groq:llama-3.3-70b-versatile",
+    mcp_servers = [mcp_fetch_server]
+)
+
+# main sync function
+async def main():
+    async with agent.run_mcp_servers():
+        result = await agent.run("extract the content and summarize it: https://blogs.nvidia.com/blog/what-are-foundation-models/")
+        output = result.output
+        return output
+
+# run the main function
+if __name__ == "__main__":
+    output = asyncio.run(main())
+    print(output) 
